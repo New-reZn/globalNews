eb08c1faa26ac13092f9b7d317d46e465ce0118b
DEL.NUL.NUL.SOH <youdontknowme3509@gmail.com>
2022-11-08 11:46:37 +0530

    0.0.3
    made imports offline and compressed there js

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from flask import Flask, jsonify, render_template, request
 from flask_restful import Resource, Api
+import requests
+from bs4 import BeautifulSoup
 
 app = Flask(__name__,static_folder="static")
 api = Api(app)
@@ -8,11 +10,11 @@ api = Api(app)
 def index():
     return render_template("index.html")
 
-class Square(Resource):
-    def get(self, num):
-        return jsonify({'data': num**2})
+# class Square(Resource):
+#     def get(self, num):
+#         return jsonify({'data': num**2})
   
-api.add_resource(Square, '/square/<int:num>')
+# api.add_resource(Square, '/square/<int:num>'
 
 
 if __name__ == '__main__':
--- static/main.js
+++ static/main.js
@@ -1,8 +1,8 @@
 'use strict';
-import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.121.1/examples/jsm/controls/OrbitControls.js';
-import { CSS2DRenderer, CSS2DObject } from 'https://threejs.org/examples/jsm/renderers/CSS2DRenderer.js';
 
-import * as THREE from 'three';
+import * as THREE from './Three.js';
+import { OrbitControls } from './orbitcontrols.js';
+import { CSS2DRenderer, CSS2DObject } from './CSS2DRenderer.js';
 
 const vertexShader = /*glsl*/ `
 varying vec2 vertexUV;
@@ -41,6 +41,7 @@ void main() {
 } 
 `;
 
+
 function calcPosFromLatLonRad(lat, lon, radius) {
     const phi = (90 - lat) * (Math.PI / 180);
     const theta = (lon + 180) * (Math.PI / 180);
@@ -61,6 +62,7 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setPixelRatio(window.devicePixelRatio);
 document.body.appendChild(renderer.domElement);
 
+//globe here
 const globe = new THREE.Mesh(
     new THREE.SphereGeometry(5, 50, 50),
     new THREE.ShaderMaterial({
@@ -82,8 +84,9 @@ textrender.domElement.style.top = `0px`;
 textrender.domElement.style.pointerEvents = `none`;
 document.body.appendChild(textrender.domElement);
 
+//atmosphere here
 const atmosphere = new THREE.Mesh(
-    new THREE.SphereGeometry(5, 50, 50),
+    new THREE.SphereGeometry(4.5, 50, 50),
     new THREE.ShaderMaterial({
         vertexShader: vertexShaderAtmos,
         fragmentShader: fragmentShaderAtmos,
@@ -93,9 +96,10 @@ const atmosphere = new THREE.Mesh(
 );
 
 atmosphere.name = 'atmosphere';
-
 atmosphere.scale.set(1.5, 1.5, 1.5);
+scene.add(atmosphere);
 
+//stars here
 const stargeo = new THREE.BufferGeometry();
 const starmat = new THREE.PointsMaterial({ color: 0xffffff });
 
@@ -110,40 +114,56 @@ for (let i = 0; i < 1000; i++) {
 stargeo.setAttribute('position', new THREE.Float32BufferAttribute(starvertices, 3));
 
 const star = new THREE.Points(stargeo, starmat);
+scene.add(star);
+
+scene.add(globe);
 
 //pin here---------------------------------------------------
 
-const pin = new THREE.Mesh(
-    new THREE.SphereGeometry(0.09, 50, 50),
-    new THREE.MeshBasicMaterial({ color: 0xff0000 })
-);
-pin.name = "pin"
+function objectViewBlocked(object) {
+    const direction = new THREE.Vector3();
+    direction.subVectors(camera.position, object.position);
+    direction.normalize();
+    const raycaster = new THREE.Raycaster(object.position, direction);
+    const intersects = raycaster.intersectObjects(scene.children, false);
+    console.log(document.getElementById(`${id}`));
+    const textobject = document.getElementById(`${object.name}`);
+    if (intersects[0].object.name == `globe` || textobject != null) {
+        textobject.style.animationName = `deletion`;
+        textobject.addEventListener('animationend', () => {
+            object.remove(object.children[0]);
+            globe.remove(object);
+        });
+        return true;
+    }
+    return false;
+}
+
+function createpin(longitude, latitude, message, size) {
+    const pin = new THREE.Mesh(
+        new THREE.SphereGeometry(0.09, 50, 50),
+        new THREE.MeshBasicMaterial({ color: 0xff0000 })
+    );
+    let latlon = new THREE.Vector3(...calcPosFromLatLonRad(longitude, latitude, 5));
+    pin.position.set(...latlon)
+    pin.name = message.toString();
+    globe.add(pin);
+    objectViewBlocked(pin);
+    const div = document.createElement('div');
+    div.textContent = message;
+    div.style.fontSize = size == undefined ? "0.5em" : size;
+    div.setAttribute('id', message)
+    div.classList.add('text');
+    const textobject = new CSS2DObject(div);
+    textobject.name = message;
+    pin.add(textobject);
+    return pin;
+}
+
 
 let coords = [40.7142700, -74.0059700];
-let latlon = new THREE.Vector3(...calcPosFromLatLonRad(coords[0], coords[1], 5));
-pin.position.x = latlon.x;
-pin.position.y = latlon.y;
-pin.position.z = latlon.z;
 
-const div = document.createElement('div');
-div.textContent = `our earth is mf suffering and you are laughing why?`;
-div.style.backgroundColor = `black`;
-div.style.fontWeight = `800`;
-div.style.color = `white`;
-div.style.fontSize = `${(Math.random() * 10)+4}px`;
-const textobject = new CSS2DObject(div);
-pin.add(textobject);
-
-function removepin(e) {
-    e.remove(e.children[0]);
-    globe.remove(e);
-}
 //------------------------------------------------------------
-
-scene.add(star);
-scene.add(atmosphere);
-scene.add(globe);
-globe.add(pin);
 
 function setCanvasDimensions(canvas, width, height, set2dTransform = false) {
     const ratio = window.devicePixelRatio
@@ -166,26 +186,14 @@ window.addEventListener('resize', () => {
     setCanvasDimensions(renderer.domElement, width, height);
 });
 
-function objectViewBlocked(object) {
-    const direction = new THREE.Vector3();
-    direction.subVectors(camera.position, object.position);
-    direction.normalize();
-    const raycaster = new THREE.Raycaster(object.position, direction);
-    const intersects = raycaster.intersectObjects(scene.children, false);
-    if (intersects[0].object.name == `globe`) {
-        removepin(object);
-    }
-}
 
 const controls = new OrbitControls(camera, renderer.domElement);
-controls.minDistance = 7;
-controls.maxDistance = 18;
+controls.minDistance = 7.5;
+controls.maxDistance = 15;
 controls.autoRotate = true;
 controls.autoRotateSpeed = 1;
 camera.position.set(0, 0, 10);
 controls.update();
-
-//setInterval(() => { objectViewBlocked(pin) }, 2000)
 
 function animate() {
     requestAnimationFrame(animate);
@@ -194,4 +202,11 @@ function animate() {
     controls.update();
 }
 
+function getinfo() {
+    fetch().then().then().catch();
+}
+
 animate();
+
+//let pin = createpin(coords[0], coords[1], "There's a new meme from the weeknd's super bowl halftime show, and its quite literally dizzying");
+//setInterval(() => { objectViewBlocked(pin) }, 2000);
